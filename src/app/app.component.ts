e3ff64e9971a7a1951e3f346042c4ffb1bb8278e
David Delbert Clough <daviddclough@yahoo.com>
2020-03-08 17:41:21 -0500

    Add functionality and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,80 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pw';
+
+  password = '';
+  length =0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  numbers: string = "123456789";
+  letters: string = "abcdefghijklmnopqrstuvwxyz";
+  symbols: string = "!@#$%^&*()_";
+
+  onChangeLength(value: string) {
+
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+    else
+    {
+      this.length = 0;
+    }
+
+    console.log(`Length is : ${this.length}`)
+  };
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  };
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  };
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  };
+
+  getPassword() {
+    var availablechars = "";
+    this.password = "";
+    if (this.includeLetters){
+      availablechars += this.letters;
+    }
+      
+    if (this.includeNumbers){
+      availablechars += this.numbers;
+    }
+      
+    if (this.includeSymbols){
+      availablechars += this.symbols;
+    }
+
+    console.log(availablechars);
+
+     var i = this.length - 1; 
+    var randchar = "";
+
+    if (this.length > 0)
+    {
+      for (i; i >= 0; i--)
+      {
+        randchar = availablechars.charAt(getRandomInt(availablechars.length));
+        this.password += randchar;
+        //console.log(randchar);
+      }
+    } 
+  }
+
+  onButtonClick() {
+    this.getPassword();
+  }
+}
+
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
